d0f5fd4633acd4fe993bee49b5f7f650d6dd6de5
hank.yuanju@gmail.com <runfree2010>
2018-10-02 00:05:44 -0400

    initial commit

---
+++ QRCodeScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCodeScanner
+//
+//  Created by Hank Cheng on 10/2/18.
+//  Copyright © 2018 Hank Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
